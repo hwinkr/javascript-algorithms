c30b45ff53bed12a79a86e2f5bd10f3f3f0076f9
hwinkr <dnddl8280@naver.com>
2023-11-23 15:13:55 +0900

    2023.11.23 solved

---
+++ Programmers/Level 2/주차 요금 계산-리팩토링.js
@@ -0,0 +1,41 @@
+const IN = "IN";
+const OUT = "OUT";
+const LAST_TIME = "23:59";
+const FULL_TIME = 1439;
+
+const convertTime = (time) => {
+  const [hour, minute] = time.split(":");
+  return Number(hour) * 60 + Number(minute);
+};
+
+function solution(fees, records) {
+  const [basicTime, basicCost, unitTime, unitCost] = fees;
+
+  const parkingLog = {};
+  records.forEach((parkingData) => {
+    const [time, carNumber, state] = parkingData.split(" ");
+    const convertedTime = convertTime(time);
+    if (state === IN) {
+      parkingLog.hasOwnProperty(carNumber)
+        ? (parkingLog[carNumber] += FULL_TIME - convertedTime)
+        : (parkingLog[carNumber] = FULL_TIME - convertedTime);
+    } else {
+      parkingLog[carNumber] -= FULL_TIME - convertedTime;
+    }
+  });
+
+  console.log(parkingLog);
+
+  const costList = [];
+  Object.entries(parkingLog).forEach(([carNumber, parkingTime]) => {
+    if (parkingTime <= basicTime) {
+      costList.push([carNumber, basicCost]);
+      return;
+    }
+    const totalCost =
+      basicCost + Math.ceil((parkingTime - basicTime) / unitTime) * unitCost;
+    costList.push([carNumber, totalCost]);
+  });
+
+  return costList.sort((c1, c2) => c1[0] - c2[0]).map((v) => v[1]);
+}
---
+++ Programmers/Level 2/주차 요금 계산.js
@@ -0,0 +1,53 @@
+const IN = "IN";
+const OUT = "OUT";
+const LAST_TIME = "23:59";
+
+const convertTime = (time) => {
+  const [hour, minute] = time.split(":");
+  return Number(hour) * 60 + Number(minute);
+};
+
+const calcTotalTime = (timeList) => {
+  if (timeList.length % 2) timeList.push(convertTime(LAST_TIME));
+  const totalTime = timeList.reduce((acc, currTime, index) => {
+    if (index % 2) return acc;
+    return acc + timeList[index + 1] - currTime;
+  }, 0);
+
+  return totalTime;
+};
+
+function solution(fees, records) {
+  const [basicTime, basicCost, unitTime, unitCost] = fees;
+
+  const timeBoard = {};
+  records.forEach((parkingData) => {
+    const [time, carNumber, state] = parkingData.split(" ");
+    const convertedTime = convertTime(time);
+    if (!timeBoard.hasOwnProperty(carNumber)) {
+      timeBoard[carNumber] = [convertedTime];
+      return;
+    }
+    timeBoard[carNumber] = [...timeBoard[carNumber], convertedTime];
+  });
+
+  const inOutTimeList = Object.keys(timeBoard)
+    .sort()
+    .map((carName) => timeBoard[carName]);
+
+  console.log(inOutTimeList);
+
+  const costList = [];
+  inOutTimeList.forEach((timeList) => {
+    const totalTime = calcTotalTime(timeList);
+    if (totalTime <= basicTime) {
+      costList.push(basicCost);
+      return;
+    }
+    const totalCost =
+      basicCost + Math.ceil((totalTime - basicTime) / unitTime) * unitCost;
+    costList.push(totalCost);
+  });
+
+  return costList;
+}
